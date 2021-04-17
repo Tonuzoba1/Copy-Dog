34c861671f0a017590edf5de439485410b55958d
Tonuzoba1 <81702143+Tonuzoba1@users.noreply.github.com>
2021-04-17 22:37:30 +0200

    2d commit

---
+++ Copy Dog Projekt/Assets/controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d")){
+            rb.AddForce(0, 0, 30);
+        }
+
+        Vector3 camVec = new Vector3(0,0,0);
+
+        cam.transform.Rotate(camVec);
+    }
+}
---
+++ Copy Dog Projekt/Assets/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float Force = 10f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(transform.up*Force);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            //rb.AddForce(-10, 0,);
+        }
+    }
+}
