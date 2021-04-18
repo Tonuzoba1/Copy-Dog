2d5a6b4d868603b7d339ed7a58d7c5c7ef06abd6
Tonuzoba1 <81702143+Tonuzoba1@users.noreply.github.com>
2021-04-18 20:56:55 +0200

    Mozgás alapok + spawn
    Finomítottam a mozgáson és hozzáadtam egy alap spawnoló rendszert

---
+++ Copy Dog Projekt/Assets/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject allyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = GameObject.Find("Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            Instantiate(allyPrefab, spawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+}
